15a8e258e9f7dce253fc71cb4487f04f40295802
Gunish <gunishsharma20@gmail.com>
2023-06-06 15:18:28 -0600

    trying to make requests work but getting error

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -2,11 +2,20 @@ const ToDo = require('../models/todo')
 
 exports.createItem = async (req,res) =>{
     try {
-        const createdTodo =  await ToDo.create(req.body)
-        res.json({createdTodo})
+        const createdTodo = new ToDo.create(req.body)
+        await createdTodo.save()
+        res.send({createdTodo})
 
     } catch (error) {
         res.json({message: error.message})
     }
 }
 
+exports.getAllItems = async (req,res) =>{
+    try {
+        const allItems = await ToDo.find({})
+        res.json(allItems)
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const todoController = require('../controllers/todosController')
 
+router.get('/todos', todoController.getAllItems)
 router.post('/todos', todoController.createItem)
 //router.get('/todos/:id', todoController.getItem)
 //router.put('/todos/:id', todoController.updateItem)
