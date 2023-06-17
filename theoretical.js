43742f1a745cb29bc95756edfcb9dc8373c8e167
Gunish <gunishsharma20@gmail.com>
2023-06-16 18:45:32 -0600

    written finished theoretical left

--- theoretical.js
+++ theoretical.js
@@ -4,8 +4,11 @@
 
 function lengthOfString(str, len = 0) {
     // If the length is 0 (base case), return len (0).
+    if(str.length === 0){
+      return len = 0
+    }
     // if (str.length === 0) return len;
-    if (!str.length) return len;
+    else if (!str.length) return len;
   
     // Remove the first letter of the string
     let restOfString = str.substring(1);
@@ -13,44 +16,60 @@ function lengthOfString(str, len = 0) {
     // Call function again, with updated string and len
     return lengthOfString(restOfString, ++len);
   }
+
+  lengthOfString('Gunish')
   
-  function sumOfArray(arr, sum =0 ,count = 0) {
+  function sumOfArray(arr, sum = 0 ,count = 0) { //
     // This function returns the sum of all of the numbers in a given array.
    
     if(count>=arr.length){
-      return 
+      console.log('The sum of this array is:', sum)
+      return sum
     } else {
       sum = sum + arr[count]
       count++
-      console.log(sum)
+      
       sumOfArray(arr, sum, count++)
     }
   }
 sumOfArray([1,2,3,4,5,6,7])
   
-  function findMax(arr,largestNum = 0, index=0) {
+  function findMax(arr, largestNum = 0, index=0) {
+
     // This function returns the largest number in a given array.
-    if(index === arr.length){
-      return 
+    if(index >= arr.length){
+      console.log('largest number is: ',largestNum)
+      return largestNum
     } else{
       if(arr[index]> largestNum){
         largestNum = arr[index]
       }
-      console.log('largest number is: ', largestNum)
-      console.log('Index is: ', index)
+      //console.log('largest number is: ', largestNum)
+      //console.log('Index is: ', index)
       index++
       findMax(arr,largestNum,index)
     }
 
-    return largestNum 
+      
   }
   
 findMax([3,4,1,5,66,2])
 
-  function factorial() {
-    // This function returns the factorial of a given number.
-  }
+  function factorial(N,f=0,count=0) {
   
+    // This function returns the factorial of a given number.
+    if(N===0){
+      return 
+    }else{
+      f = N*(N-1) //f =20
+      count = count + f //count = 20
+      console.log('count is', count)
+      factorial(N-1,f,count)
+    }
+  }
+
+factorial(5)
+
   function fibonacci() {
     // This function returns the Nth number in the fibonacci sequence.
     // https://en.wikipedia.org/wiki/Fibonacci_number
@@ -65,22 +84,51 @@ findMax([3,4,1,5,66,2])
     // ["HH", "HT", "TH", "TT"]
     // H stands for Heads and T stands for tails
     // Represent the two outcomes of each flip as "H" or "T"
+    const bigArr = []
+    let h = 'H'
+    let t = 'T'
+    if(false){
 
+    }else{
+      let flipArr = []
+
+      for(let i =0; i<N; i++){
+        let chance = Math.floor(Math.random() * 2);
+        //console.log('chance is ', chance)
+        if(chance === 0){
+          flipArr.push(h)
+          
+        } else if(chance === 1){
+          flipArr.push(t)
+        }
+        
+        bigArr.push(flipArr)
+      }
+
+      console.log(bigArr)
+      //coinFlips(N)
+    }
 
   }
-  
+  coinFlips(2)
 
   function letterCombinations() {
     // This function returns an array of all combinations of the given letters
     // Input type: Array of single characters
     // For example, letterCombinations(["a","b","c"]) would return the following:
     // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+    if(){
+
+    }else{
+      
+    }
+
   }
   
   module.exports = {
-    lengthOfString,
-    sumOfArray,
-    findMax,
+    lengthOfString, //done
+    sumOfArray,// done
+    findMax, //done 
     factorial,
     fibonacci,
     coinFlips,
